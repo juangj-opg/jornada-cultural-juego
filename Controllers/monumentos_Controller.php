801ac9c088357cb18ac891025c22843058289ef6
JuanGJ <juangj1b@gmail.com>
2022-02-10 12:16:05 +0100

    Controlador base

--- Controllers/monumentos_Controller.php
+++ Controllers/monumentos_Controller.php
@@ -2,8 +2,36 @@
 function inicio(){
     require "Models/clases_Model.php"; 
 
-    $clases = getClases();
+    $clases = getClases(); // Para recoger el nombre y la ID de las clases
 
     include "Views/monumentos_Inicio.php";
 }
+
+function puntuaciones(){
+    require "Models/clases_Model.php"; 
+
+    $clases = getClases(); // Para recoger el nombre y la puntuación de cada clase
+
+    include "Views/monumentos_Puntuaciones.php";
+}
+
+function ruleta(){
+    require "Models/clases_Model.php"; 
+
+    // Enviar por POST el número de opciones de la ruleta del "login" del Inicio.
+
+    // Con ese número, configurar la ruleta en otra función
+
+    include "Views/monumentos_Ruleta.php";
+}
+
+function ahorcado(){
+    require "Models/clases_Model.php"; 
+
+    // La ruleta le enviará por GET la id de provincia y con eso, 
+    // recogerá una palabradicha provincia en la base de datos.
+
+    include "Views/monumentos_Ahorcado.php";
+}
+
 ?>
