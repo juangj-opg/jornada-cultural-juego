44c8b8074892a24e93bce484f8880bb22d849efb
Farricius <jose.hernandez.chacon.al@iespoligonosur.org>
2022-02-18 11:48:54 +0100

    Corrección inicio

--- Views/monumentos_Ruleta.php
+++ Views/monumentos_Ruleta.php
@@ -15,25 +15,24 @@
 <body>
     <!-- Formulario Login Kid-->
 
-    <div class="row">
-        <div class="col-lg-12 text-center">
-            <div class="titular text-light">El conejo ahorcado</div>
-        </div>
-    </div>
+    <!-- BANDERA -->
 
     <div class="row">
         <div class="col-md-6">
             <div class="login-form">
                 <form action="/examples/actions/confirmation.php" method="post">
                     <div class="form-group">
-                        <input type="text" pattern="[a-Z]" class="form-control" placeholder="Introduce tu nombre!" required="required">
+                        <input type="text" style = "background-color: white" pattern="[a-Z]" class="form-control text-center" placeholder="Introduce tu nombre" required="required">
                     </div>
 
                     <div class="form-group">
-                        <select name="clase" class="form-control" placeholder="Clase..." required="required">
-                            <option hidden selected>Clase</option>
+                        <select name="clase" style = "background-color: white"  class="form-control text-center" placeholder="Clase..." required="required">
+                            <option value="vacio" selected disabled> Elige tu clase </option>
+                            <?php foreach($clases as $clase):?>
+                            <option value="<?php echo $clase['id']; ?>"><?php echo $clase['clase']; ?></option>
+                            <?php endforeach; ?>
                         </select>
-                        <small id="emailHelp">* ¡Hay un ranking con las mejores clases!</small>
+                        <small id="emailHelp"><br>* ¡Hay un ranking con las mejores clases! 🥇🥈🥉</small>
                     </div>
 
 
@@ -44,8 +43,6 @@
                     <div class="d-flex justify-content-center">
                         <button type="button" id="instrucciones" class="btn btn-primary">INSTRUCCIONES</button>
                     </div>
-
-
                 </form>
             </div>
         </div>
@@ -56,7 +53,7 @@
                 <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
                     <rect width="100%" height="100%" fill="#777" data-darkreader-inline-fill="" style="--darkreader-inline-fill:#5a6165;"></rect><text x="50%" y="50%" fill="#777" dy=".3em" data-darkreader-inline-fill="" style="--darkreader-inline-fill:#9d9488;">140x140</text>
                     <image height="140" width="140" xlink:href="https://amaga.es/image/cache/catalog/productos/banderas%20y%20mastiles/banderas%20autonomicas/andalucia-800x800.jpg" clip-path="url(#myCircle)" />
-                </svg> elkiwi además de una fruta es un tipo de ave 
+                </svg>
                 <br>
                 <br>
                 <div class="guia">
@@ -64,8 +61,8 @@
                     <p>¡Gira la ruleta!</p>
                     <p>Debes adivinar un monumento de esa ciudad andaluza...</p>
                     <p>¡Pero deberás de hacerlo letra a letra!</p>
-                    <p>Pulsa en una letra para intentar acertarlo</p>
-                    <p>Puedes usar una pista ;)</p>
+                    <p>Clickea en una letra para intentar acertarlo</p>
+                    <p>Puedes que haya pistas ;)</p>
                 </div>
                 </p>
             </div>
@@ -102,7 +99,7 @@
             border-radius: 1%;
             border-width: 4px;
             border-style: solid;
-            background-color: yellow;
+            background: url('https://upload.wikimedia.org/wikipedia/commons/6/63/Flag_of_Andalusia_%28simple%29.svg');
             padding: 50px;
         }
 
@@ -116,20 +113,19 @@
         }
 
         .form-control {
-            font-size: 54px;
+            font-size: 38px;
+            padding: 3px;
+    
         }
 
         .login-form {
             background-color: greenyellow;
             padding: 30px;
         }
-
-        button {
-            height: 150px;
-            width: 150px;
-        }
-
+        
         .btn {
+            height: 100px;
+            width: 100px;
             margin-top: 30px;
             padding: 10px 10px;
             font-size: 32px;
@@ -158,6 +154,7 @@
             font-size: 24px;
             text-align: center;
         }
+
     </style>
 
 </body>
