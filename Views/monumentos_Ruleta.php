f9e01a8099301df36a7457c90065c325bb9c75be
Farricius <jose.hernandez.chacon.al@iespoligonosur.org>
2022-02-18 13:36:31 +0100

    Ruleta basada en 360º funcional

--- Views/monumentos_Ruleta.php
+++ Views/monumentos_Ruleta.php
@@ -16,20 +16,22 @@
             <!-- OCCIDENTAL EXAMPLE -->
             <div class="box1">
                 <span class="span1"><b>SEVILLA</b></span>
-                <span class="span2"><b>HUELVA</b></span>
+                <span class="span2"><b>CÁDIZ</b></span>
                 <span class="span3"><b>CÓRDOBA</b></span>
-                <span class="span4"><b>CÁDIZ</b></span>
+                <span class="span4"><b>HUELVA</b></span>
+
             </div>
             <!-- ORIENTAL EXAMPLE -->
             <div class="box2">
-                <span class="span1"><b>MÁLAGA</b></span>
-                <span class="span2"><b>ALMERÍA</b></span>
+                <span class="span2"><b>MÁLAGA</b></span>
+                <span class="span1"><b>JAÉN</b></span>
                 <span class="span3"><b>GRANADA</b></span>
-                <span class="span4"><b>JAÉN</b></span>
+                <span class="span4"><b>ALMERÍA</b></span>
+
             </div>
         </div>
 
-        <button class="spin" onclick="myfunction()">GIRAR</button>
+        <button class="spin" id="girar" onclick="myfunction()">GIRAR</button>
     </div>
 
 
@@ -41,15 +43,19 @@
 <script>
     function myfunction() {
 
+        document.getElementById("girar").removeAttribute("onclick");
 
-        var x = 1024; //min value
-        var y = 9999; // max value
+        var x = 1080; //min value is 3 vueltas
+        var y = 1440; // max value is 4 vueltas
 
         var deg = Math.floor(Math.random() * (x - y)) + y;
+        var gradosFinales = deg - 1080;
+
+        //enviar ciudades  con _POST  ajax
 
         document.getElementById('box').style.transform = "rotate(" + deg + "deg)";
-
         var element = document.getElementById('mainbox');
+
         element.classList.remove('animate');
         setTimeout(function() {
             element.classList.add('animate');
@@ -64,6 +70,7 @@
         margin: 0;
         outline: none;
     }
+
 
     body {
         font-family: Open Sans;
@@ -113,7 +120,7 @@
 
     .span1 {
         clip-path: polygon(0 92%, 100% 50%, 0 8%);
-        background-color: #fffb00;
+        background-color: green;
         top: 120px;
         left: 0;
 
@@ -121,21 +128,21 @@
 
     .span2 {
         clip-path: polygon(100% 92%, 0 50%, 100% 8%);
-        background-color: #ff4fa1;
+        background-color: darkgreen;
         top: 120px;
         right: 0;
     }
 
     .span3 {
         clip-path: polygon(50% 0%, 8% 100%, 92% 100%);
-        background-color: #ffaa00;
+        background-color: greenyellow;
         bottom: 0;
         left: 120px;
     }
 
     .span4 {
+        background-color: lime;
         clip-path: polygon(50% 100%, 92% 0, 8% 0);
-        background-color: #22ff00;
         top: 0;
         left: 120px;
     }
@@ -171,7 +178,6 @@
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-
     }
 
     .spin {
@@ -182,19 +188,19 @@
         width: 95px;
         height: 95px;
         border-radius: 50%;
-        border: 4px solid #fff;
+        border: 4px solid black;
         background-color: #001aff;
         color: #fff;
         box-shadow: 0 5px 20px #000;
         font-weight: bold;
-        font-size: 22px;
+        font-size: 24px;
         cursor: pointer;
     }
 
     .spin:active {
         width: 70px;
         height: 70px;
-        font-size: 20px;
+        font-size: 18px;
     }
 
     .mainbox.animate:after {
