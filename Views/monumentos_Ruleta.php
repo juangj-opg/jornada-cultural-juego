8e6ef98f010ef4ef03a110ac1325ef379613ffbc
Farricius <jose.hernandez.chacon.al@iespoligonosur.org>
2022-02-20 21:08:22 +0100

    Ruleta por si acaso...

--- Views/monumentos_Ruleta.php
+++ Views/monumentos_Ruleta.php
@@ -36,7 +36,7 @@
     <br>
     <br>
 
-    <img id="flechita" src="./Views/images/Arrow.png" alt="flecha de prueba" width="100" height="100">
+    <img id="flechita" src="./Content/Images/Arrow.png" alt="flecha de prueba" width="100" height="100">
 
 
     <!--Contenedor Ruleta-->
@@ -83,29 +83,29 @@
 
         // Almacenamos en cookie -> id Provincia
         if (gradosFinales >= 0 && gradosFinales < 45) {
-            alert("Huelva!");
-            document.cookie = "provicia=4";
+            // alert("Huelva!");
+            document.cookie = "provincia=4";
         } else if (gradosFinales >= 45 && gradosFinales < 90) {
-            alert("Málaga!");
-            document.cookie = "provicia=5";
+            // alert("Málaga!");
+            document.cookie = "provincia=5";
         } else if (gradosFinales >= 90 && gradosFinales < 135) {
-            alert("Sevilla!");
-            document.cookie = "provicia=2";
+            // alert("Sevilla!");
+            document.cookie = "provincia=2";
         } else if (gradosFinales >= 135 && gradosFinales < 180) {
-            alert("Almería!");
-            document.cookie = "provicia=6";
+            // alert("Almería!");
+            document.cookie = "provincia=6";
         } else if (gradosFinales >= 180 && gradosFinales < 225) {
-            alert("Córdoba!");
-            document.cookie = "provicia=3";
+            // alert("Córdoba!");
+            document.cookie = "provincia=3";
         } else if (gradosFinales >= 225 && gradosFinales < 270) {
-            alert("Jaén!");
-            document.cookie = "provicia=8";
+            // alert("Jaén!");
+            document.cookie = "provincia=8";
         } else if (gradosFinales >= 270 && gradosFinales < 315) {
-            alert("Cádiz!");
-            document.cookie = "provicia=7";
+            // alert("Cádiz!");
+            document.cookie = "provincia=7";
         } else {
-            alert("Granada!");
-            document.cookie = "provicia=1";
+            // alert("Granada!");
+            document.cookie = "provincia=1";
         }
 
         // Animaciones
@@ -120,17 +120,17 @@
 
         const myTimeout = setTimeout(cambioBoton, 2100);
 
+        // Cambia a "jugar"
         function cambioBoton() {
             $("button").css("background-color", "red");
             $("button").css("color", "white");
             $("button").text("JUGAR");
-            document.getElementById("girar").addAttribute("onclick");
-            $("button").attr("onclick","cambioJugar()");
+            document.getElementById("girar").setAttribute("onclick", "cambioJugar()");
         }
+    }
 
-        function cambioJugar() {
-            location.href = "index.php?controller=monumentos&action=ahorcado";
-        }
+    function cambioJugar() {
+        location.href = "index.php?controller=monumentos&action=ahorcado";
     }
 </script>
 
@@ -333,13 +333,5 @@
 
     .botonBloqueado {
         background-color: gray;
-    }
-
-    .runrun {
-        display: block;
-        position: absolute;
-        margin-left: 45%;
-        margin-right: auto;
-        width: 50%;
     }
 </style>
